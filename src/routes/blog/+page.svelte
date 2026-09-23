dfd4c83e0d470eed3eb6291e4b0da5ed9bd309c0
Mauricio C <maujcardenas@gmail.com>
2026-09-23 17:40:23 -0500

    colors blue

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -83,7 +83,7 @@
         margin-bottom: 4rem;
     }
     .card {
-        background-color: #f8f3f2;
+        background-color: #00233d;
         border: 1px solid #71717a;
         border-radius: 12px;
         overflow: hidden;
